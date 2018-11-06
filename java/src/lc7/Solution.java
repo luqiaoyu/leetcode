92060bfa1fbbfe0bb5672c40de922543d6331198
luqiaoyu <luqiaoyu@sjtu.edu.cn>
2018-11-06 16:42:12 +0800

    lc7 Reverse Integer

---
+++ java/src/lc7/Solution.java
@@ -0,0 +1,20 @@
+package lc7;
+
+/**
+ * Created by luqiaoyu on 2018/11/6.
+ * 7. Reverse Integer
+ * 14ms 100%
+ */
+public class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
