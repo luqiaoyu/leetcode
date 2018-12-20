34c4ee2084c669d4445db7b2bfe10c9457bd94cf
luqiaoyu <luqiaoyu@sjtu.edu.cn>
2018-12-20 20:12:27 +0800

    lc 64Minimum Path Sum

--- java/src/lc64/Solution.java
+++ java/src/lc64/Solution.java
@@ -2,7 +2,26 @@ package lc64;
 
 /**
  * 64. Minimum Path Sum
+ * 7ms 35.24%
  */
 public class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
 
+        int[] dp = grid[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] += dp[i - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = Math.min(dp[j - 1], dp[j]) + grid[i][j];
+            }
+        }
+
+        return dp[n - 1];
+    }
 }
